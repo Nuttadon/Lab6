2a36b01bf94eaa79bf037da792cfef43056157db
Nuttadon <nuttadon_p@cmu.ac.th>
2021-11-26 17:50:48 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
-
+double deg2rad(double inde){
+    return (inde*M_PI)/180;
+}
+double rad2deg(double inra){
+    return 90-(inra*(180/M_PI));
+    // if(inra =! 0){
+    // return 90-(inra*(180/M_PI));
+    // }
+    // else{
+    //     return 0;
+    // } สำหรับแก้ไขบัคเมื่อ ใส่องศาเป็น 0 ทั้งหมดแล้วผลขององศาลัพธ์จะออกมาเป็น 90 (แต่ใส่ในตัวทดลองแล้วผิด)
+        
+}
+double findXComponent(double le1,double le2,double an1,double an2){
+    return le1*sin(an1) + le2*sin(an2);
+}
+double findYComponent(double le1,double le2,double an1,double an2){
+    return le1*cos(an1) + le2*cos(an2);
+}
+double pythagoras(double x,double y){
+    return sqrt((x*x)+(y*y));
+}
+void showResult(double l,double d){
+  cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+  cout << "Length of the resultant vector = " <<l<<endl;
+  cout << "Direction of the resultant vector (deg) = "<<d<<endl;
+  cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
